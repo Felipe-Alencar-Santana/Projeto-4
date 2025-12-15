cf5d11b0b7c7ca2a4a68750980a20ef20c699a17
Felipe <Felipe@teste.com.br>
2025-12-15 17:22:17 -0300

    primeiro commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const cleanCSS = require('gulp-clean-css');
+const replace = require('gulp-replace');
+const uglify = require('gulp-uglify');
+
+function script (){
+    return gulp.src('src/script/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
+}
+
+function styles() {
+    return gulp.src('src/styles/main.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(cleanCSS({ level: 2 }))
+        .pipe(replace(/\n+$/, ''))
+        .pipe(gulp.dest('./dist/css'));
+}
+
+function images() {
+    return gulp.src('src/imagem/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/imagem'));
+}
+
+exports.default = gulp.parallel(styles, images, script);
+
+exports.watch = function() {
+    gulp.watch('src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('src/script/*.js', gulp.parallel(script));
+}
